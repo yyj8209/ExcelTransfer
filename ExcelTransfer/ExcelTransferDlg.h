d983d97dce19788f08758e147d6f8b1781fe0731
yyj8209 <yyj8209@163.com>
2020-06-06 21:22:06 +0800

    first commit

---
+++ ExcelTransfer/ExcelTransferDlg.h
@@ -0,0 +1,32 @@
+
+// ExcelTransferDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CExcelTransferDlg 对话框
+class CExcelTransferDlg : public CDialogEx
+{
+// 构造
+public:
+	CExcelTransferDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_EXCELTRANSFER_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
